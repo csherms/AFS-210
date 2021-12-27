1f8b88665c00e91d48f28302dfcb9a76adba9518
csherms <casey.sherman@bryanuniversity.edu>
2021-12-27 16:14:16 -0700

    first_commit

--- week-5/sorting/mergesort.py
+++ week-5/sorting/mergesort.py
@@ -1,46 +1,45 @@
-def mergeSort(nlist):
-    print("Splitting ",nlist)
-    # insert your code to complete the mergeSort function
-    if len(nlist) == 1:
-        return nlist
-    mid_point = int((len(nlist))/2)
+def merge_sort(unsorted_list): 
+        print("Sorting: ", unsorted_list)
+        if len(unsorted_list) == 1: 
+            return unsorted_list 
+
+        mid_point = int((len(unsorted_list))/2) 
+
+        first_half = unsorted_list[:mid_point] 
+        second_half = unsorted_list[mid_point:] 
+
+        half_a = merge_sort(first_half) 
+        half_b = merge_sort(second_half) 
         
-    first_half = nlist[:mid_point]
-    second_half = nlist[mid_point:]
-    
-    half_a = mergeSort(first_half)
-    half_b = mergeSort(second_half)
+        print("Merging: ",unsorted_list)
+        return merge(half_a, half_b)
 
-    print("Merging ",nlist)
-    return merge(nlist, half_a, half_b)
+
+
+def merge(first_sublist, second_sublist): 
+        i = j = 0 
+        merged_list = [] 
+
+        while i < len(first_sublist) and j < len(second_sublist): 
+            if first_sublist[i] < second_sublist[j]: 
+                merged_list.append(first_sublist[i]) 
+                i += 1 
+            else: 
+                merged_list.append(second_sublist[j]) 
+                j += 1 
+
+        while i < len(first_sublist): 
+            merged_list.append(first_sublist[i]) 
+            i += 1 
+
+        while j < len(second_sublist): 
+            merged_list.append(second_sublist[j]) 
+            j += 1 
+
+        return merged_list
+        
+
    
-    
+myList = [55,  31,  26,  20,  63,  7,  51,  74,  81,  40] 
 
-def merge(nlist,lefthalf,righthalf):
-    i=j=k=0       
-    while i < len(lefthalf) and j < len(righthalf):
-        if lefthalf[i] < righthalf[j]:
-            nlist[k]=lefthalf[i]
-            i=i+1
-        else:
-            nlist[k]=righthalf[j]
-            j=j+1
-        k=k+1
-
-    while i < len(lefthalf):
-        nlist[k]=lefthalf[i]
-        i=i+1
-        k=k+1
-
-    while j < len(righthalf):
-        nlist[k]=righthalf[j]
-        j=j+1
-        k=k+1
-    return nlist
-
-
-
-myList = [55,  31,  26,  20,  63,  7,  51,  74,  81,  40]
-
-
-mergeSort(myList)
+print("Sorted List: " + str(merge_sort(myList)))
