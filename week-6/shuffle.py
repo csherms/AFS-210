93ccda846685f482e801456b1fa5ea7ea01c7d5d
csherms <casey.sherman@bryanuniversity.edu>
2022-01-06 16:50:57 -0700

    first_commit

--- week-6/shuffle.py
+++ week-6/shuffle.py
@@ -1,18 +1,23 @@
 import random
 
 my_list = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
-print("Before Shuffle: ", my_list)
 
 
-# Algorithm time complexity = O(n)
-for i in range(len(my_list)-1, 0, -1):
-    
-    # Generating a random index from 0 to i + 1
-    j = random.randint(0, i + 1)
+def shuffle_algorithm(my_list):
+        # Fisher Yates Algorithm time complexity = O(n)
+    for i in range(len(my_list)-1, 0, -1):
+        # Generating a random index from 0 to i + 1
+        r = random.randint(0, i + 1)
+        # Swap i with the element at random index
+        my_list[i], my_list[r] = my_list[r], my_list[i]
+    return my_list
 
-    # Swap i with the element at random index
-    my_list[i], my_list[j] = my_list[j], my_list[i]
 
-print("After shuffle: ", my_list)
+print(my_list)
+print(shuffle_algorithm(my_list))
+print(shuffle_algorithm(my_list))
+print(shuffle_algorithm(my_list))
+print(shuffle_algorithm(my_list))
+
 
 
