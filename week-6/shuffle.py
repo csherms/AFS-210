96b0e508f7f4936a8aea127b27a0b16799ec3f62
csherms <casey.sherman@bryanuniversity.edu>
2022-01-06 17:57:56 -0700

    first_commit

--- week-6/shuffle.py
+++ week-6/shuffle.py
@@ -7,7 +7,7 @@ def shuffle_algorithm(my_list):
         # Fisher Yates Algorithm time complexity = O(n)
     for i in range(len(my_list)-1, 0, -1):
         # Generating a random index from 0 to i + 1
-        r = random.randint(0, i + 1)
+        r = random.randint(0, i)
         # Swap i with the element at random index
         my_list[i], my_list[r] = my_list[r], my_list[i]
     return my_list
