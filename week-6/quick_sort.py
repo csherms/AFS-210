b312f8756e70aed879c3c97545b8bf1b6e401ac4
csherms <casey.sherman@bryanuniversity.edu>
2022-01-06 17:16:42 -0700

    first_commit

--- week-6/quick_sort.py
+++ week-6/quick_sort.py
@@ -17,31 +17,55 @@ def quick_sort(a_list, start, end):
 def partitionStart(a_list, start, end):
     #selects the first value on the list (index 0)
     return partition(a_list, start, end)
+# First Run Time: 0.002032041549682617
+# Second Run Time: 0.001001119613647461
+# Third Run Time: 0.0009996891021728516  Fastest
 
-# ----------------------  This is where we're working... 
+
+
+# ---------------------------------------------------------------------  Start of changes...
 # Swapped the start and ending values
 def partitionEnd(a_list, start, end):
-    # Swaps index 0 value with index 1 value
     a_list[start], a_list[end -1] = a_list[end-1], a_list[start]
-    #returning partiton will then get index 0's new value
     return partition(a_list, start, end)
+# First Run Time: 0.0019989013671875
+# Second Run Time: 0.002001523971557617
+# Third Run Time: 0.0010013580322265625  Fastest
 
 # Using the middle value as pivot
 def partitionMiddle(a_list, start, end):
     mid_point = int(len(a_list)) //2
     a_list[start], a_list[mid_point] = a_list[mid_point], a_list[start]
     return partition(a_list, start, end)
+# First Run Time: 0.0019991397857666016
+# Second Run Time: 0.0019979476928710938  Fastest
+# Third Run Time: 0.0020020008087158203
 
 # Selecting a random index in the list as a pivot
 def partitionAtRandom(a_list, start, end):
     ran_index = random.randint(0,len(a_list)-1)
     a_list[start], a_list[ran_index] = a_list[ran_index], a_list[start]
     return partition(a_list, start, end)
+# First Run Time: 0.002991199493408203  Fastest
+# Second Run Time: 0.0030002593994140625
+# Third Run Time: 0.0029985904693603516
+
+
+# Fastest times from each different pivot selection after 3 runs each:
+# start    0.0009996891021728516
+# end      0.0010013580322265625
+# middle   0.0019979476928710938
+# random   0.002991199493408203
+
+# Fastest Overall is the pivot selection at the start.
+
+# ---------------------------------------------------------------- End of changes
+
+
 
 def partition(a_list, start, end):
     # Select the first element as our pivot point
     pivot = a_list[start]
-# ----------------------------------------End of changes
     
     # Start at the first element past the pivot point
     low = start + 1
